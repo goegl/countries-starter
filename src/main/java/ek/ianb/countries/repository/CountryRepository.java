80c10ea7c9ba1644241506bb438c82da91e5ff1a
Andreas Lysholm Jensen <goegl12@gmail.com>
2026-03-12 13:36:32 +0100

    necessary changes

--- src/main/java/ek/ianb/countries/repository/CountryRepository.java
+++ src/main/java/ek/ianb/countries/repository/CountryRepository.java
@@ -3,6 +3,7 @@ package ek.ianb.countries.repository;
 import ek.ianb.countries.model.Country;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Repository;
 
 import java.sql.*;
@@ -10,12 +11,12 @@ import java.util.List;
 
 @Repository
 public class CountryRepository {
-//    @Value("${spring.datasource.url}")
-//    private String dbUrl;
-//    @Value("${spring.datasource.username}")
-//    private String username;
-//    @Value("${spring.datasource.password}")
-//    private String password;
+    @Value("${spring.datasource.url}")
+    private String dbUrl;
+    @Value("${spring.datasource.username}")
+    private String username;
+    @Value("${spring.datasource.password}")
+    private String password;
 
     private final JdbcTemplate jdbcTemplate;
 
@@ -23,9 +24,21 @@ public class CountryRepository {
         this.jdbcTemplate = jdbcTemplate;
     }
 
-    public List<Country> findAll() {
+    private final RowMapper<Country> rowMapper = (rs, rowNum) -> {
+        Country country = new Country(rs.getInt("id"), rs.getString("name"));
+//        country.setId(rs.getInt("id"));
+//        country.setName(rs.getString("name"));
+        return country;
+    };
+
+//    public List<Country> findAll() {
+//        String sql = "SELECT id, name FROM countries";
+//        return jdbcTemplate.query(sql, new CountryRowMapper());
+//    }
+
+    public List<Country> findAll(){
         String sql = "SELECT id, name FROM countries";
-        return jdbcTemplate.query(sql, new CountryRowMapper());
+        return jdbcTemplate.query(sql, rowMapper);
     }
 }
 
